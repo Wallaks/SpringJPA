bc6dc2e3ea8027b42046ff15adb4e266aad8697e
Wallaks Cardoso da Silva <42080153+Wallaks@users.noreply.github.com>
2023-04-21 15:17:09 -0300

    Initial commit

---
+++ src/main/java/com/TreinamentoSpring/SpringJPA/Emprego.java
@@ -0,0 +1,54 @@
+package com.TreinamentoSpring.SpringJPA;
+
+import org.springframework.lang.NonNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
+@Entity // dizer que quero criar uma base de dados
+public class Emprego {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO) // primary key autoIncrement
+	private long id;
+
+	@NonNull
+	@Size(min = 4)
+	private String titulo;
+	private String empresa;
+	private String descricao;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getEmpresa() {
+		return empresa;
+	}
+
+	public void setEmpresa(String empresa) {
+		this.empresa = empresa;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+}
